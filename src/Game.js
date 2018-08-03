d4a3687deb5a8305fb59b2bb448c971d349aae86
rosaxny <rosaxny@gmail.com>
2018-08-03 15:44:24 -0500

    add event listener (can update state from input)

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import GuessHistory from './GuessHistory';
 
+const randomNumber = '987';
+
 export default class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state ={
+			guess: ''
+		}
+		this.handleGuess = this.handleGuess.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleGuess(e) {
+		e.preventDefault();
+		if(this.state.guess === randomNumber) {
+			console.log('yay!')
+		} else {
+			console.log('boooo');
+		}
+		// value is a string
+	}
+	handleChange(e) {
+		this.setState({guess: e.target.value});
+		// value is a string
+	}
 	render() {
+		const { guess } = this.state
 		return (
 			<div className="game">
-				<form>
-					<input type="text" />
+				<form onSubmit={this.handleGuess}>
+					<input 
+						type="text"
+						value={ guess }
+						onChange={this.handleChange}
+					/>
 					<br />
 					<button type="submit">Guess!</button>
 				</form>
--- src/GuessHistory.js
+++ src/GuessHistory.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default class GuessHistory extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			guess: 0
+		}
+	}
 	render() {
 		return (
 			<div>
-				<p>Guess #1</p>
+				<p>Guess #{this.state.guess}</p>
 			</div>
 		);
 	}
