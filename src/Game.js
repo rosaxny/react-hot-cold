10cd3cd6e0e8f836b91061781a076df3c3f26199
rosaxny <rosaxny@gmail.com>
2018-08-05 02:30:58 -0500

    add functions & conditional rendering (game works)

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import GuessHistory from './GuessHistory';
 
-const randomNumber = '987';
+const randomNumber = '97';
 
 export default class Game extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state ={
-			guess: ''
+			guess: '',
+			history: [],
+			submitted: false
 		}
 		this.handleGuess = this.handleGuess.bind(this);
 		this.handleChange = this.handleChange.bind(this);
@@ -15,71 +16,77 @@ export default class Game extends React.Component {
 	
 	handleGuess(e) {
 		e.preventDefault();
-		if(this.state.guess === randomNumber) {
-			console.log('yaaay!');		
-		} else {
-			console.log('boooo');
+		this.setState({ submitted: true });
+		const { guess, history } = this.state;
+		if(guess !== randomNumber) {	
+			this.setState({ history: history.concat(guess)});
+			// WHY DOES THIS NOT CONSOLE LOG THE UPDATED ARRAY - always a nuber behind
+			this.setState({submitted:false, guess: ''});
+
 		}
 		// value is a string
 	}
 	handleChange(e) {
-		this.setState({guess: e.target.value});
+		this.setState({ guess: e.target.value });
 		// value is a string
 	}
 	render() {
-		const { guess } = this.state;
-		if(this.state.guess === '') {
+		const { guess, history, submitted } = this.state;
+		let list = history.join(', ');
+		if (submitted === false) {
 			return (
 				<div className="game">
 					<form onSubmit={this.handleGuess}>
 						<input 
-							type="text"
+							type="number"
 							value={ guess }
-							onChange={this.handleChange}
+							onChange={(e) => this.handleChange(e)}
+							min={-100}
+							max={100}
 						/>
 						<br />
 						<button type="submit">Guess!</button>
 					</form>
-					<GuessHistory />
-				</div>
-			);
-		} else if (guess !== randomNumber) {
-			console.log('boooo');
-			return (
-				<div className="game">
-					<form onSubmit={this.handleGuess}>
-						<input 
-							type="text"
-							value={ guess }
-							onChange={this.handleChange}
-						/>
-						<br />
-						<button type="submit">Guess!</button>
-					</form>
-					<GuessHistory />
-					<p>{guess}</p>
+					<h2>Guess #{history.length}</h2>
+					<p>{list}</p>
 				</div>
 			);
 		} else {
-			console.log('yaaay');
-
-			return (
-				<div className="game">
-					<p>Correct!</p>
-					<form onSubmit={this.handleGuess}>
-						<input 
-							type="text"
-							value={ guess }
-							onChange={this.handleChange}
-						/>
-						<br />
-						<button type="submit">Guess!</button>
-					</form>
-					<GuessHistory />
-					<p>{guess}</p>
-				</div>
-			);
+			if (guess !== randomNumber) {
+				return  (
+					<div className="game">
+						<form onSubmit={this.handleGuess}>
+							<input 
+								type="number"
+								value={ guess }
+								onChange={(e) => this.handleChange(e)}
+								min={-100}
+								max={100}
+							/>
+							<br />
+							<button type="submit">Guess!</button>
+						</form>
+						<h2>Guess #{history.length}</h2>
+						<p>{list}</p>
+					</div>
+				);
+			} else {
+				return  (
+					<div className="game">
+						<p>Correct! The number was {randomNumber}!</p>
+						<form onSubmit={this.handleGuess}>
+							<input 
+								type="number"
+							/>
+							<br />
+							<button type="submit">Guess!</button>
+						</form>
+						<h2>Guess #{history.length}</h2>
+						<p>{list}</p>
+					</div>
+				);
+			}
 		}
-		
+
 	}
 }
